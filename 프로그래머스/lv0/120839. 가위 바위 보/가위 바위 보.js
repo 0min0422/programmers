2c078c98ad23db5c8ce6de2f470fb1c2691d7c6d
Young Min Yoon <93363272+0min0422@users.noreply.github.com>
2023-03-24 16:01:01 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,11 @@
+function solution(rsp) {
+    var answer = '';
+    for(let x of rsp) {
+        if(x === '0'){
+            answer += "5"
+        }else if(x === '2') {
+            answer += '0'
+        }else answer += '2'
+    }
+    return answer;
+}
