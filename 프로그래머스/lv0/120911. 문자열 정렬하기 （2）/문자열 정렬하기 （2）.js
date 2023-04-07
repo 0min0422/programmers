8dc81b5e18bb9478105e95057d73ff4643672593
Young Min Yoon <93363272+0min0422@users.noreply.github.com>
2023-04-07 16:06:43 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -0,0 +1,10 @@
+function solution(str) {
+    var answer = '';
+    var ans = [];
+    str= str.toLowerCase();
+    ans = [...str]
+    ans = ans.sort().join("");
+    
+    //answer = str.
+    return ans;
+}
