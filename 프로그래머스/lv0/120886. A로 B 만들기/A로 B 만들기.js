17ac3bf6a8afba1b1c0e978d6b6303862d11b78d
Young Min Yoon <93363272+0min0422@users.noreply.github.com>
2023-04-08 22:48:13 +0900

    [level 0] Title: A로 B 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,12 @@
+function solution(before, after) {
+    var answer = 0;
+    var before = [...before].sort().join('');
+    var after = [...after].sort().join('');
+    if(before === after) answer = 1
+        
+    
+    
+    
+    
+    return answer;
+}
